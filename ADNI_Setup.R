70d24ccdccf7234ed3fa18a951d32ccbbbc7ba95
dianahobbs <dianah@wustl.edu>
2022-04-14 13:07:37 -0500

    renamed folder and created OASIS/ADNI setup files

---
+++ ADNI_Setup.R
@@ -0,0 +1,12 @@
+# Title: ADNI Cohort Organization for Data Science Project
+# Cleaning 
+# Author: Diana Hobbs
+# Date: April 2022
+
+###### Set Up ######
+# Load packages
+pacman::p_load(tidyverse,jtools,naniar,lubridate,janitor,zoo)
+
+###### Load data and select columns of interest ###### 
+
+###### ADNI Cohort Organization ######
