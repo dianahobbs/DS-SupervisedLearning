d76d41a8eb4482a1426e2fd4d91ae2456ea6f29b
dianahobbs <dianah@wustl.edu>
2022-04-14 21:33:19 -0500

    setup files complete

--- ADNI_Setup.R
+++ ADNI_Setup.R
@@ -3,6 +3,10 @@
 # Author: Diana Hobbs
 # Date: April 2022
 
+# Functions to set up environment
+#rm(list=ls()) #removes all variables from the current environment
+options(scipen = 999) #forces R not to use scientific notation
+
 ###### Set Up ######
 # Load packages
 pacman::p_load(tidyverse,jtools,naniar,lubridate,janitor,zoo)
@@ -10,3 +14,139 @@ pacman::p_load(tidyverse,jtools,naniar,lubridate,janitor,zoo)
 ###### Load data and select columns of interest ###### 
 
 ###### ADNI Cohort Organization ######
+# demographics 
+demo <- read.csv("./Data/ADNI/PTDEMOG.csv")%>%
+  replace_with_na(replace=list(PTGENDER=c(-4), PTEDUCAT=c(-1,-4), PTRACCAT=c(-4)))%>%
+  mutate(PTDOBDD = 1,
+         BIRTH = make_date(PTDOBYY,PTDOBMM,PTDOBDD))%>%
+  #PTGENDER: 1=Male; 2=Female
+  mutate(SEX = str_replace_all(PTGENDER, c("1"="Male", "2"="Female")))%>%
+  #PTRACCAT: 1=American Indian or Alaskan Native; 2=Asian; 3=Native Hawaiian or Other Pacific Islander;
+  #          4=Black or African American; 5=White; 6=More than one race; 7=Unknown - replace with NA
+  mutate(PTRACCAT = str_replace_all(PTRACCAT,c("1"="non-White", "2"="non-White","3"="non-White","4"="non-White","5"="White","6"="non-White","7"="Unknown")))%>%
+  rename(EDUC=PTEDUCAT, RACE=PTRACCAT)%>%
+  group_by(RID)%>%
+  filter(row_number()==1)%>%
+  ungroup(RID)%>%
+  dplyr::select(RID,BIRTH,SEX,EDUC,RACE)
+
+# apoe
+gene <- read.csv("./Data/ADNI/APOERES.csv")%>%
+  mutate(APOE = paste(APGEN1, APGEN2, sep=""))%>%
+  # APOE: 0=non-carrier of e4 allele, 1=carrier of e4 allele
+  mutate(APOE4 = str_replace_all(APOE,c("22"="0", "33"="0", "23"="0", "32"="0",
+                                        "24"="1", "42"="1", 
+                                        "34"="1", "43"="1", "44"="1")))%>%
+  dplyr::select(RID,APOE4)
+
+# Merge Demo & Gene
+data <- merge(demo, gene, by="RID", all=F)
+rm(demo,gene)
+
+# BMI - USERDATE has more data AND the dates are within a year of EXAMDATE !!*** USE USERDATE !!!!*****
+##****first open in excel and format USERDATE and EXAMDATE as 3/14/2012 and THEN save to csv
+BMI <- read.csv("Data/ADNI/VITALS.csv")%>%
+  mutate(BMI_Date = as.Date(USERDATE, "%m/%d/%Y"))%>%
+  replace_with_na(replace=list(VSWEIGHT=c(-1),VSWTUNIT=c(-1),VSHEIGHT=c(-1,-4),VSHTUNIT=c(-1,-4),VSBPSYS=c(-1)))%>%
+  group_by(RID)%>%
+  fill(VSHEIGHT, .direction = "down")%>%
+  fill(VSHEIGHT, .direction = "up")%>%
+  fill(VSHTUNIT, .direction = "down")%>%
+  fill(VSHTUNIT, .direction = "up")%>%
+  ungroup(RID)%>%
+  mutate(BMI = case_when(VSHTUNIT == "1" & VSWTUNIT == "1" ~ (VSWEIGHT/(VSHEIGHT^2)*703),
+                         VSHTUNIT == "1" & VSWTUNIT == "2" ~ ((VSWEIGHT * 2.20462)/(VSHEIGHT^2)*703),
+                         VSHTUNIT == "2" & VSWTUNIT == "1" ~ (VSWEIGHT/((VSHEIGHT/2.54)^2)*703),
+                         VSHTUNIT == "2" & VSWTUNIT == "2" ~ ((VSWEIGHT * 2.20462)/((VSHEIGHT/2.54)^2)*703)))%>%
+  filter(VISCODE2 != 'f')%>%
+  dplyr::select(RID,VISCODE2,BMI_Date,BMI)
+
+data<- merge(data, BMI, by="RID", all=FALSE)
+rm(BMI)
+
+# MRI ***first open in excel and format USERDATE and EXAMDATE as 3/14/2012 and THEN save to csv
+ADNI1GO2_MR <- read.csv("./Data/ADNI/UCSFFSX51_11_08_19.csv")%>%
+  mutate(MR_Date = as.Date(EXAMDATE, "%m/%d/%Y"),
+         tothipp =ST29SV + ST88SV,
+         ICV = ST10CV,
+         percenthipp = ((tothipp*100)/ICV),
+         # thickness = average of entorhinal, fusiform, inferior temporal, and middle temporal 
+         thick = ((ST24TA + ST83TA + ST26TA + ST85TA + ST32TA + ST91TA + ST40TA + ST99TA)/8))%>%
+  filter(VISCODE2 != 'f', OVERALLQC == "Pass")%>%
+  dplyr::select(RID,MR_Date,tothipp,percenthipp,ICV,thick)%>%
+  na.omit()
+
+ADNI3_MR <- read.csv("./Data/ADNI/UCSFFSX6_03_02_21.csv")%>%
+  mutate(MR_Date = as.Date(EXAMDATE, "%m/%d/%Y"),
+         tothipp =ST29SV + ST88SV,
+         ICV = ST10CV,
+         percenthipp = ((tothipp*100)/ICV),
+         # thickness = average of entorhinal, fusiform, inferior temporal, and middle temporal 
+         thick = ((ST24TA + ST83TA + ST26TA + ST85TA + ST32TA + ST91TA + ST40TA + ST99TA)/8))%>%
+  filter(VISCODE2 != 'f', OVERALLQC == "Pass")%>%
+  dplyr::select(RID,MR_Date,tothipp,percenthipp,ICV,thick)%>%
+  na.omit()
+
+mri <- rbind(ADNI1GO2_MR,ADNI3_MR)        
+rm(ADNI1GO2_MR,ADNI3_MR)        
+ 
+# av45 - Centiloid conversion = 163.6*av45_3060_SUVR-181.0 Amyloid Positivity 21.9 cutoff 
+av45 <- read.csv("./Data/ADNI/UCBERKELEYAV45_01_14_21.csv")%>%
+  mutate(amy_Date = as.Date(EXAMDATE, "%m/%d/%Y"),
+         av45_fsuvr_tot_cortmean = ((CTX_LH_PRECUNEUS_SUVR + CTX_RH_PRECUNEUS_SUVR +
+                                       CTX_LH_SUPERIORFRONTAL_SUVR + CTX_RH_SUPERIORFRONTAL_SUVR +
+                                       CTX_LH_ROSTRALMIDDLEFRONTAL_SUVR + CTX_RH_ROSTRALMIDDLEFRONTAL_SUVR +
+                                       CTX_LH_LATERALOCCIPITAL_SUVR + CTX_RH_LATERALOCCIPITAL_SUVR + 
+                                       CTX_LH_MEDIALORBITOFRONTAL_SUVR + CTX_RH_MEDIALORBITOFRONTAL_SUVR +
+                                       CTX_LH_SUPERIORTEMPORAL_SUVR + CTX_RH_SUPERIORTEMPORAL_SUVR + 
+                                       CTX_LH_MIDDLETEMPORAL_SUVR + CTX_RH_MIDDLETEMPORAL_SUVR)/14),
+         centiloid = 163.6 * av45_fsuvr_tot_cortmean - 181.0,
+         AmyPositivity = case_when(centiloid >= 21.9 ~ "A+",
+                                   centiloid < 21.9 ~ "A-"))%>%
+  dplyr::select(RID,amy_Date,centiloid,AmyPositivity)
+
+image <- merge(mri,av45, by="RID", all=T)%>%
+  mutate(mriamy_DIFF = abs(as.numeric(difftime(MR_Date,amy_Date, unit="weeks"))/52.25))%>%
+  filter(between(mriamy_DIFF, 0, 1))%>%
+  dplyr::select(-c(mriamy_DIFF,amy_Date)) 
+rm(mri,av45)
+
+data <- merge(data,image,by="RID",keep.all=T)%>%
+  mutate(mribmi_DIFF = abs(as.numeric(difftime(MR_Date,BMI_Date, unit="weeks"))/52.25),
+         AGE = as.numeric(difftime(MR_Date, BIRTH, unit="weeks"))/52.25)%>%
+  filter(between(mribmi_DIFF, 0, 1))%>%
+  #group_by(RID)%>%
+  #arrange(MR_Date)%>%
+  #filter(row_number()==1)%>%
+  #ungroup()%>%
+  dplyr::select(-c(mribmi_DIFF,BMI_Date,BIRTH)) 
+rm(image)
+
+# CDR - USERDATE has more data AND the dates are within a year of EXAMDATE !!*** USE USERDATE !!!!*****
+##****first open in excel and format USERDATE and EXAMDATE as 3/14/2012 and THEN save to csv
+CDR <- read.csv("./Data/ADNI/CDR.csv")%>%
+  mutate(CDR_Date = as.Date(USERDATE, "%m/%d/%Y"))%>%
+  replace_with_na(replace=list(CDGLOBAL=c(-1)))%>%
+  mutate(CDR = case_when(CDGLOBAL == "0" ~ "0", CDGLOBAL>0 ~ "1"))%>%
+  filter(VISCODE2 != 'f')%>%
+  dplyr::select(RID,CDR_Date,VISCODE2,CDR)
+
+# MMSE ***first open in excel and format USERDATE as 3/14/2012 and THEN save to csv
+MMSE <- read.csv("./Data/ADNI/MMSE.csv")%>%
+  mutate(MMSE_Date = as.Date(USERDATE, "%m/%d/%Y"))%>%
+  filter(VISCODE2 != 'f')%>%
+  replace_with_na(replace=list(MMSCORE=c(-1)))%>%
+  rename(MMSE=MMSCORE)%>%
+  select(RID,MMSE_Date,VISCODE2,MMSE)
+
+cog <- merge(CDR,MMSE,by=c("RID","VISCODE2"))%>%
+  mutate(cdrmmse_DIFF = abs(as.numeric(difftime(MMSE_Date,CDR_Date, unit="weeks"))/52.25))%>%
+  filter(between(cdrmmse_DIFF, 0, 1))%>%
+  dplyr::select(-c(cdrmmse_DIFF,MMSE_Date))
+rm(CDR,MMSE)
+
+ADNI_data <- merge(data,cog,by=c("RID","VISCODE2"),all=F)%>%
+  group_by(RID)%>% # Grouping by ID & keeping only distinct CDR visits for each participant
+  distinct(CDR_Date, .keep_all = T)%>%
+  ungroup()
+rm(cog,data)
--- Data_Setup.R
+++ Data_Setup.R
@@ -1,5 +1,5 @@
-# Title: OASIS and ADNI Cohort Organization for Data Science Project
-# Cleaning and merging
+# Title: OASIS and ADNI Cohort Merge for Data Science Project
+# Merging
 # Author: Diana Hobbs
 # Date: April 2022
 
@@ -7,61 +7,64 @@
 # Load packages
 pacman::p_load(tidyverse,jtools,naniar,lubridate,janitor,zoo)
 
-###### Load data and select columns of interest ###### 
+###### Load data and Merge ###### 
+source("OASIS_Setup.R")
+source("ADNI_Setup.R")
 
-###### OASIS Cohort Organization ######
-# demographics/clinical/genetic/cognitive
-demo_OASIS <- read.csv("./Data/ClinDataFull.csv")%>%
-  group_by(Subject)%>%
-  fill(height, .direction="down")%>%
-  fill(height, .direction="up")%>%
-  mutate(Apoe4 = str_replace_all(apoe,c("22"="0", "33"="0", "23"="0", "32"="0",
-                                        "24"="1", "42"="1", 
-                                        "34"="1", "43"="1", "44"="1")),
-         Race = str_replace_all(Race,c("Asian"="non-White","African American"="non-White","Caucasian"="White")),
-                  BMI = (weight/(height^2)*703))%>%
-  filter(dx1 != "nonAD Dementia", dx1 != "nonAD")%>%
-  replace_with_na(replace = list(dx1=c("",".")))%>%
-  mutate(cdr = case_when(cdr == 0 ~ "0",
-                         cdr>0 ~ "1"),
-         Study = case_when(M.F == "M" | M.F == "F" ~ "OASIS"))%>%
-  rename(Sex=M.F, Dx=dx1,MMSE=mmse,CDR=cdr)%>%
-  dplyr::select(Label, Study,Subject, Age, Sex, Education, Race, BMI, Apoe4, MMSE, CDR)%>%
-  na.omit()
+# Organize the data 
+OASIS_data <- OASIS_data%>%
+  rename(days_since_mri=Label)%>%
+  dplyr::select(RID,AGE,SEX,EDUC,RACE,BMI,APOE4,tothipp,percenthipp,ICV,thick,centiloid,AmyPositivity,CDR,MMSE,days_since_mri)
 
-## mri 
-mri <- read.csv("./Data/MRI.csv")
-mri_id <- mri%>%rename(ICV=IntraCranialVol)%>%dplyr::select(Label,Subject,ICV)
+ADNI_data <- ADNI_data%>%
+  mutate(days_since_mri=abs(as.numeric(difftime(MR_Date,CDR_Date,units = "days"))))%>%
+  dplyr::select(RID,AGE,SEX,EDUC,RACE,BMI,APOE4,tothipp,percenthipp,ICV,thick,centiloid,AmyPositivity,CDR,MMSE,days_since_mri)
 
-# extract volumes 
-mri_vol <- mri[,grepl("_volume", names(mri))]
-mri_vol <- cbind(mri_id, mri_vol)
-mri_vol <- mri_vol%>%
-  mutate(tothipp = Right.Hippocampus_volume + Left.Hippocampus_volume)%>%
-  mutate(percenthipp = ((tothipp*100)/ICV))%>%
-  dplyr::select(Label,Subject,tothipp,percenthipp,ICV)%>%
-  na.omit()
+# Create a baseline dataset for each 
+OASIS_Base_data <- OASIS_data%>%
+  group_by(RID)%>%
+  arrange(days_since_mri)%>%
+  filter(row_number()==1)%>%
+  ungroup()%>%
+  filter(CDR==0 & MMSE>=27)
 
-mri_thick <-  mri[,grepl("_thickness", names(mri))]
-mri_thick <- cbind(mri_id, mri_thick)
-mri_thick <- mri_thick%>%
-  mutate(thick = ((((lh_entorhinal_thickness + rh_entorhinal_thickness)+
-                     (lh_fusiform_thickness + rh_fusiform_thickness)+
-                     (lh_inferiortemporal_thickness + rh_inferiortemporal_thickness)+
-                     (lh_middletemporal_thickness + rh_middletemporal_thickness))/8)))%>%
-  dplyr::select(Label,Subject,thick)%>%
-  na.omit()
+ADNI_Base_data <- ADNI_data%>%
+  group_by(RID)%>%
+  arrange(days_since_mri)%>%
+  filter(row_number()==1)%>%
+  ungroup()%>%
+  filter(CDR==0 & MMSE>=27)
+  
+# Figure out which IDs in full dataset are in the full dataset 
+OASIS_Base_IDs <- as.data.frame(table(OASIS_Base_data$RID))
+ADNI_Base_IDs <- as.data.frame(table(ADNI_Base_data$RID))
 
-mri <- merge(mri_vol,mri_thick,by=c("Label","Subject"))
-rm(mri_id,mri_thick,mri_vol)
+OASIS_data_KEEP <- OASIS_data[(OASIS_data$RID %in% OASIS_Base_IDs$Var1),]
+ADNI_data_KEEP <- ADNI_data[(ADNI_data$RID %in% ADNI_Base_IDs$Var1),]
 
-## pet 
-centiloid <- read.csv("./Data/centiloid.csv")%>%
-  rename(centiloid = Centil_fSUVR_rsf_TOT_CORTMEAN,
-         Label = MRId)
 
-data <- merge(demo,mri,by=c("Label","Subject"))
-data <- merge(data,centiloid,by="Label")
+
+
+
+data <- rbind(OASIS_data,ADNI_data)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 rm(demo,centiloid,mri)
 
 data1<-data%>%
@@ -85,100 +88,5 @@ table(data3$CDR)
 #########################
 ########################
 ########################
-
-
-###### ADNI Cohort Organization ######
-
-# demographics 
-demo_ADNI <- read.csv("./Data/ADNIMERGE_May15.2014.csv")%>%
-  mutate(PTGENDER = str_replace_all(PTGENDER, c("Male"="M", "Female"="F")))%>%
-  #PTRACCAT: 1=American Indian or Alaskan Native; 2=Asian; 3=Native Hawaiian or Other Pacific Islander;
-  #          4=Black or African American; 5=White; 6=More than one race; 7=Unknown
-  mutate(PTRACCAT = str_replace_all(PTRACCAT,c("Am Indian/Alaskan"="non-White","Asian"="non-White","Hawaiian/Other PI"="non-White",
-                                               "Black"="non-White","5"="White")),
-         Study = case_when(PTGENDER == "M" | PTGENDER == "F" ~ "ADNI"))%>%
-  filter(PTRACCAT != "More than one")%>%
-  rename(Age=AGE,Sex=PTGENDER,Education=PTEDUCAT,Race=PTRACCAT)%>%
-  select(RID,EXAMDATE,Study,Age,Sex,Education,Race,Hippocampus,ICV,MMSE)%>%
-  na.omit()%>%
-  group_by(RID)%>%
-  arrange(EXAMDATE)%>%
-  filter(row_number()==1)%>%
-  ungroup()
-
-# apoe
-apoe_ADNI <- read.csv("./Data/APOERES.csv")%>%
-  mutate(apoe = paste(APGEN1, APGEN2, sep=""))%>%
-  mutate(Apoe4 = str_replace_all(apoe, c("22"="0", "33"="0", "23"="0", "32"="0",
-                                         "24"="1", "42"="1",
-                                         "34"="1", "43"="1", "44"="1")))%>%
-  dplyr::select(RID,Apoe4)
-
-# Merge demo & apoe
-data_ADNI <- merge(demo_ADNI, apoe_ADNI, by="RID", all=FALSE)
-rm(demo_ADNI,apoe_ADNI)
-
-# BMI - USERDATE has more data AND the dates are within a year of EXAMDATE !!*** USE USERDATE !!!!*****
-##****first open in excel and format USERDATE and EXAMDATE as 3/14/2012 and THEN save to csv
-BMI_ADNI <- read.csv("./Data/VITALS.csv")%>%
-  mutate(USERDATE = as.Date(USERDATE, "%m/%d/%Y"))%>%
-  mutate(EXAMDATE = as.Date(EXAMDATE, "%m/%d/%Y"))%>%
-  replace_with_na(replace=list(VSWEIGHT=c(-1),VSWTUNIT=c(-1),VSHEIGHT=c(-1,-4),VSHTUNIT=c(-1,-4),VSBPSYS=c(-1)))%>%
-  mutate(BMI = case_when(VSHTUNIT == "1" & VSWTUNIT == "1" ~ (VSWEIGHT/(VSHEIGHT^2)*703),
-                         VSHTUNIT == "1" & VSWTUNIT == "2" ~ ((VSWEIGHT * 2.20462)/(VSHEIGHT^2)*703),
-                         VSHTUNIT == "2" & VSWTUNIT == "1" ~ (VSWEIGHT/((VSHEIGHT/2.54)^2)*703),
-                         VSHTUNIT == "2" & VSWTUNIT == "2" ~ ((VSWEIGHT * 2.20462)/((VSHEIGHT/2.54)^2)*703)))%>%
-  filter(VISCODE2 != 'f')%>%
-  filter(VISCODE2 == 'sc')%>%
-  dplyr::select(RID,VISCODE2,BMI)%>%
-  na.omit()
-
-data_ADNI <- merge(data_ADNI, BMI_ADNI, by="RID", all=FALSE)
-  
-# CDR - USERDATE has more data AND the dates are within a year of EXAMDATE !!*** USE USERDATE !!!!*****
-##****first open in excel and format USERDATE and EXAMDATE as 3/14/2012 and THEN save to csv
-CDR_ADNI <- read.csv("./Data/CDR.csv")%>%
-  mutate(USERDATE = as.Date(USERDATE, "%m/%d/%Y"))%>%
-  replace_with_na(replace=list(CDGLOBAL=c(-1)))%>%
-  mutate(CDR = case_when(CDGLOBAL == "0" ~ "0", CDGLOBAL>0 ~ "1"))%>%
-  filter(VISCODE2 != 'f')%>%
-  dplyr::select(RID,USERDATE,VISCODE2,CDR)
-
-data_ADNI <- merge(data_ADNI, CDR_ADNI, by=c("RID","VISCODE2"))
-rm(BMI_ADNI)
-data_ADNI <- data_ADNI%>%filter(CDR==0)
-
-CDR_Follow_ADNI <- CDR_ADNI%>%
-  filter(VISCODE2 != "sc")%>%
-  na.omit()
-
-
-IDs <- as.data.frame(table(data_ADNI$RID))
-data3 <- CDR_Follow_ADNI[(CDR_Follow_ADNI$RID %in% IDs$Var1),]
-table(data3$CDR)
-
-
-###### Cognitive Tests ######
-# MMSE ***first open in excel and format USERDATE and EXAMDATE as 3/14/2012 and THEN save to csv
-MMSE_ADNI <- read.csv("./Data/MMSE.csv")%>%
-  mutate(USERDATE = as.Date(USERDATE, "%m/%d/%Y"))%>%
-  mutate(EXAMDATE = as.Date(EXAMDATE, "%m/%d/%Y"))%>%
-  mutate(DIFF = EXAMDATE - USERDATE)%>%
-  filter(VISCODE2 != 'f')%>%
-  replace_with_na(replace=list(MMSCORE=c(-1)))%>%
-  #group_by(RID)%>%
-  #arrange(USERDATE)%>%
-  #filter(row_number()==1)%>%
-  #ungroup()%>%
-  rename(MMSE=MMSCORE)%>%
-  select(RID,VISCODE2,MMSE)
-
-data1_ADNI <- merge(data1_ADNI, MMSE_ADNI, by=c("RID","VISCODE2"), all=FALSE)%>%
-  na.omit()
-data_ADNI <- merge(data_ADNI, data1_ADNI, by="RID", all=FALSE)%>%
-  na.omit()
-rm(MMSE_ADNI)
-
-
 
 
